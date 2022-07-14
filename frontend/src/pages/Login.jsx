df506aa26070db16566e919493bf4fc1f78b4ec2
hasangoli <mrhasangoli@gmail.com>
2022-07-15 00:57:10 +0430

    Redux store and authSlice configured!

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
-import { toast } from 'react-toastify';
 import { FaSignInAlt } from 'react-icons/fa';
+import { useDispatch, useSelector } from 'react-redux';
+import { login } from '../features/auth/authSlice';
 
 const Login = () => {
   const [formData, setFormData] = useState({
@@ -9,6 +10,12 @@ const Login = () => {
   });
 
   const { email, password } = formData;
+
+  const dispatch = useDispatch();
+
+  const { user, isLodaing, isSuccess, message } = useSelector(
+    state => state.auth
+  );
 
   const onChange = e => {
     setFormData(prevState => ({
@@ -19,6 +26,13 @@ const Login = () => {
 
   const onSubmit = e => {
     e.preventDefault();
+
+    const userData = {
+      email,
+      password,
+    };
+
+    dispatch(login(userData));
   };
 
   return (
